55a07928fab99d6bb988cea9b686dc917473f66c
Hummingbird-12 <goodboyih96@gmail.com>
2019-01-06 23:17:34 +0900

    working on QRdex activity

---
+++ app/src/main/java/com/ino/qrmon/QRAdapter.java
@@ -0,0 +1,90 @@
+package com.ino.qrmon;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.lang.reflect.Field;
+
+public class QRAdapter extends BaseAdapter {
+
+    private final int ENTRY_SIZE = 100;
+    private boolean found[] = new boolean[ENTRY_SIZE];
+    private Context mContext;
+
+    // Constructor
+    public QRAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return found.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        found[4] = true;
+        ImageView imageView;
+        TextView textView;
+        String filename = "quiz" + String.valueOf(position);
+        int drawableId;
+
+        try {
+            Class res = R.drawable.class;
+            Field field = res.getField(filename);
+            drawableId = field.getInt(null);
+        }
+        catch (Exception e) {
+            Log.e("QRdex", "Failure to get drawable id.", e);
+            return null;
+        }
+
+        if (convertView == null) {
+            if(found[position]) {
+                imageView = new ImageView(mContext);
+                imageView.setLayoutParams(new GridView.LayoutParams(240, 180));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setPadding(3, 3, 3, 3);
+                imageView.setImageResource(drawableId);
+                return imageView;
+            }
+            else {
+                textView = new TextView(mContext);
+                textView.setLayoutParams(new GridView.LayoutParams(240, 180));
+                textView.setPadding(3, 3, 3, 3);
+                textView.setBackgroundColor(Color.LTGRAY);
+                textView.setGravity(Gravity.CENTER);
+                textView.setTextSize(24);
+                textView.setText(String.valueOf(position + 1));
+                return textView;
+            }
+        }
+        else {
+            if(found[position]) {
+                imageView = (ImageView) convertView;
+                imageView.setImageResource(drawableId);
+                return imageView;
+            }
+            else {
+                textView = (TextView) convertView;
+                textView.setText(String.valueOf(position + 1));
+                return textView;
+            }
+        }
+    }
+}
--- app/src/main/java/com/ino/qrmon/QRdexActivity.java
+++ app/src/main/java/com/ino/qrmon/QRdexActivity.java
@@ -2,6 +2,7 @@ package com.ino.qrmon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class QRdexActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class QRdexActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrdex);
+
+        GridView gridView = findViewById(R.id.dexContent);
+        gridView.setAdapter(new QRAdapter(this));
     }
 }
